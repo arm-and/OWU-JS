2666c2b843f279bf567625710f230594bc6a740c
arm-and <armandriy@gmail.com>
2021-04-13 23:51:08 +0300

    functions constructors,classes (HW5)

--- HW5/HW5.js
+++ HW5/HW5.js
@@ -113,8 +113,8 @@ gamePc.games();
 //===========================================================================================================
 
 class UpgradePC extends PC {
-    constructor(ram, power, name) {
-        super(ram, power, name);
+    constructor(ram, power, model) {
+        super(ram, power, model);
         this.newpower = this.power + (this.power * 0.1);
         this.newram = this.ram * 2;
     }
@@ -144,20 +144,122 @@ upgrade.newRam();
 // Якшо потужність процесора менша ніж 500. І оперативка менша за 8 потрібно видати помилку, (алерт, або консоль) що на цьому відрі ігри не запускаються.
 // ============================================================================================================
 
-// class GamerPC extends PC {
-//     constructor(ram, power, model) {
-//         super(ram, power, model);
-//         this.fps *= 2;
-//         this.power = this.power - this.power * 0.01;
-//     }
-//
-//     gameOn() {
-//         if (this.power < 500 && this.ram < 8) {
-//             return console.error('on this bucket games are not started');
-//         }
-//         console.log(`You playing with ${this.fps}FPS and nominal power ${this.power}`);
-//     }
-// }
-//
-// let gamerPC = new GamerPC(8,5000,'GameBoy');
-// gamerPC.gameOn()
+class GamerPC extends PC {
+    constructor(ram, power, model) {
+        super(ram, power, model);
+        this.fps *= 2;
+        this.power = this.power - this.power * 0.01;
+    }
+
+    gameOn() {
+        if (this.power < 500 && this.ram < 8) {
+            return console.error('on this bucket games are not started');
+        }
+        console.log(`You playing with ${this.fps}FPS and nominal power ${this.power}`);
+    }
+}
+
+let gamerPC = new GamerPC(8, 5000, 'GameBoy');
+gamerPC.gameOn()
+
+// ===============================================================================================================
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль "їдемо зі швидкістю {максимальна швидкість} на годину"
+// -- info () - яка виводить всю інформацію про автомобіль
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і доавляет його в поточний об'єкт car
+//================================================================================================================
+
+function Car(model, company, year, speed, engine) {
+    this.model = model;
+    this.company = company;
+    this.year = year;
+    this.speed = speed;
+    this.engine = engine;
+
+    this.drive = function () {
+        console.log(`їдемо зі швидкістю ${this.speed} на годину`);
+    }
+
+    this.info = function () {
+        console.log(`
+        Model: ${this.model}
+        Company: ${this.company}
+        year: ${this.year}
+        max speed: ${this.speed}
+        engine: ${this.engine}
+        driver: ${this.driver}`
+        );
+    }
+
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.speed = newSpeed;
+    }
+
+    this.changeYear = function (newValue) {
+        this.year = newValue;
+    }
+
+    this.addDriver = function (newDriver) {
+        this.driver = newDriver;
+    }
+
+}
+
+let car = new Car('X5', 'BMW', 2019, 280, 3.0);
+car.drive();
+car.info();
+car.increaseMaxSpeed(50);
+car.changeYear(2020);
+car.addDriver({name: 'Arm', age: 30});
+
+//===============================================================================================================
+// -створити класс попелюшка з полями ім'я, вік, розмір ноги
+// --Створити 10 попелюшок , покласти їх в масив
+// --Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+// -- за допоиоги циклу знайти яка попелюшка повинна бути з принцом
+//=================================================================================================================
+
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+class Prince extends Human {
+    constructor(name, age, size) {
+        super(name, age)
+        this.size = size;
+    }
+}
+
+class Princess extends Human {
+    constructor(name, age, foodSize) {
+        super(name, age);
+        this.foodSize = foodSize;
+    }
+
+}
+
+const princesses = [
+    new Princess('Margaret', 94, 38),
+    new Princess('Anna', 44, 38),
+    new Princess('Maria', 23, 39),
+    new Princess('Megan', 18, 39),
+    new Princess('Julia', 17, 38),
+    new Princess('Ashley', 22, 36),
+    new Princess('Nadia', 45, 37),
+    new Princess('Georgina', 26, 43),
+    new Princess('Jenifer', 31, 38),
+    new Princess('Samantha', 15, 39),
+]
+let prince = new Prince('Cristiano Ronaldo', 36, 43)
+
+for (const princess of princesses) {
+    if (princess.foodSize === prince.size) {
+        console.log(princess)
+    }
+}
